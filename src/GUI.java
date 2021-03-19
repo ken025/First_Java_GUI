b79957f2d051b15d50f09d93b03552bcf692ff2e
ken025 <kennethmanuel250@gmail.com>
2021-03-18 22:43:30 -0400

    Worked on sizing and set up for window

--- src/GUI.java
+++ src/GUI.java
@@ -12,7 +12,11 @@ public class GUI {
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
         panel.setLayout((new GridLayout(0, 1)));
 
-
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("Our GUI");
+        frame.pack();
+        frame.setVisible(true);
     }
 
     public static void main(String[] args){
