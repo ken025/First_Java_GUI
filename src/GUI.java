aef54739f68c688e2fe1af2845911aae15d67aae
ken025 <kennethmanuel250@gmail.com>
2021-03-18 22:56:26 -0400

    Implemented Label and Action listener to increase count on click

--- src/GUI.java
+++ src/GUI.java
@@ -1,16 +1,29 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+
+    private int count = 0;
+    private JLabel label = new JLabel("Number of clicks:  0     ");
+    private JFrame frame;
+    private JPanel panel;
 
     public GUI(){
 //        making the frame (window)
-        JFrame frame = new JFrame();
+        frame = new JFrame();
+
+//          button
+        JButton button = new JButton("Click Me!");
+        button.addActionListener(this); //action listener for click of button
 
 //        inside the window (panel)
-        JPanel panel = new JPanel();
-        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 30, 30));
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
         panel.setLayout((new GridLayout(0, 1)));
+        panel.add(button);
+        panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -21,5 +34,11 @@ public class GUI {
 
     public static void main(String[] args){
         new GUI();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        count++;
+        label.setText("Number of clicks: " + count);
     }
 }
